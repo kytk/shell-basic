4ae25b754667932b5a28793a265047f190a8e0d5
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2019-01-20 22:35:52 +0900

    add fslroi

--- data/scripts/example/gen_fa_script.sh
+++ data/scripts/example/gen_fa_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+#set -x
 
 if [ $# -lt 1 ]; then
   echo "Error! No file is specified!"
@@ -12,8 +12,10 @@ do
   dmri=$(remove_ext $f)
   #eddy current correction
   eddy_correct $dmri ${dmri}_ecc 0
+  #extract b0 image
+  fslroi ${dmri}_ecc ${dmri}_b0 0 1
   #generate mask
-  bet ${dmri}_ecc ${dmri}_brain -f 0.72 -m
+  bet ${dmri}_b0 ${dmri}_brain -f 0.3 -m
   #generate FA
   dtifit --data=${dmri}_ecc    		\
          --out=$dmri			\
